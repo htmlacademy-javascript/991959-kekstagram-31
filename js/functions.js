b3cb22e13b4fd7788a687e1b554027a7d868a1f8
July-4n <september4n@gmail.com>
2024-03-12 22:32:53 +0500

    add functions

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+const checkMaxStringLength = (string, maxLength) => string.length <= maxLength;
+
+console.log(checkMaxStringLength('проверяемая строка', 20));
+
+const isPolindrome = (string) => {
+  const newString = string.replace(/ /g, '').toLowerCase();
+
+  let reversedString = '';
+
+  for (let i = newString.length - 1; i >= 0; i--) {
+    reversedString += newString[i];
+  }
+  return newString === reversedString;
+};
+
+console.log(isPolindrome('Лёша на полке клопа нашёл '));
+
+
+// const isPolindrome = (string) => {
+//   const normalizedString = string.replace(/\s+/g, '').toLowerCase();
+
+//   return normalizedString === [...normalizedString].reverse().join('');
+// };
+
+const extractNumbers = (string) => Number(
+  [...string].filter((item) => !isNaN(parseInt(item, 10))).join('') || NaN
+);
+// const extractNumbers = (string = '') => Math.abs(parseInt(string.replace(/\D+/g, ''), 10));
+
+console.log(extractNumbers('vgb2023!1fg'));
